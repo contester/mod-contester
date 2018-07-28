81e6759794d60a0be86e5cd546791b6153623434
Lem-a-k <kazachkova.anna@gmail.com>
2018-07-28 20:00:36 +0400

    contesterID
    ids of contesters (for "all solutions") from sql

--- my_solutions.php
+++ my_solutions.php
@@ -85,28 +85,26 @@
         echo "<a href=status.php?a=$instance>".get_string('status', 'contester')."</a>";
     echo "</p>";
 
-	$thisc = get_string('thiscontester', 'contester');
-	$allc = get_string('all', 'contester');
-	//$thiscontester = "";
-	if ($thisorall == 1)
-	{
-		$thisc = "<a href=my_solutions.php?a=".$contester->id."&tha=0>".$thisc."</a>";
-	}
-   	else
-   	{
-   		//$thiscontester = " AND contester.id = ".$contester->id." ";
-   		$allc = "<a href=my_solutions.php?a=".$contester->id."&tha=1>".$allc."</a>";
-   	}
-	echo "<p><strong>".get_string('solutionlist', 'contester')." (".$thisc." \ ".$allc.")</strong></p>";
+    $thisc = get_string('thiscontester', 'contester');
+    $allc = get_string('all', 'contester');
 
-	if (!$userid) 
-	$userid = $USER->id;
+    if ($thisorall == 1)
+    {
+        $thisc = "<a href=my_solutions.php?a=".$contester->id."&tha=0>".$thisc."</a>";
+    }
+    else
+    {
+        $allc = "<a href=my_solutions.php?a=".$contester->id."&tha=1>".$allc."</a>";
+    }
+    echo "<p><strong>".get_string('solutionlist', 'contester')." (".$thisc." \ ".$allc.")</strong></p>";
 
-	//$table = null;
-	$table = new html_table();
-	$table->head = array(get_string('problem', 'contester'), get_string('prlanguage', 'contester'),
-		get_string('date'), get_string('status', 'contester'), get_string('points', 'contester'),
-		get_string('modulename', 'contester'));
+    if (!$userid) 
+        $userid = $USER->id;
+
+    $table = new html_table();
+    $table->head = array(get_string('problem', 'contester'), get_string('prlanguage', 'contester'),
+                         get_string('date'), get_string('status', 'contester'), get_string('points', 'contester'),
+                         get_string('modulename', 'contester'));
 
 	/*$sql = "
 	SELECT problems.name as p1, languages.name as p2, submits.submitted as p3,
@@ -126,59 +124,65 @@
 
 	$tmp = mysql_query($sql);*/
 	
-	if ($thisorall != 1)	
-	{
-    	$tmp = $DB->get_records_sql('SELECT submits.id as p4, problems.name as p1, languages.name as p2, submits.submitted as p3,
-    		   contester.name as p5
-    	FROM   mdl_contester_problems as problems,
-    		   mdl_contester_submits as submits,
-    		   mdl_contester_languages as languages,
-    		   mdl_contester as contester
-    	WHERE
-    		   submits.student=? AND
-    		   submits.lang=languages.id AND
-    		   submits.problem = problems.dbid AND
-    		   submits.contester = contester.id AND contester.id = ? 
-    	ORDER BY submits.submitted DESC', array($userid, $contester->id));
+    if ($thisorall != 1)	
+    {
+        $tmp = $DB->get_records_sql('SELECT submits.id as p4,
+	                                    problems.name as p1,
+					    languages.name as p2,
+					    submits.submitted as p3,
+    		                            contester.name as p5,
+					    contester.id as contesterID
+                                     FROM   mdl_contester_problems as problems,
+                                            mdl_contester_submits as submits,
+                                            mdl_contester_languages as languages,
+                                            mdl_contester as contester
+                                     WHERE
+                                            submits.student=? AND
+                                            submits.lang=languages.id AND
+                                            submits.problem = problems.dbid AND
+                                            submits.contester = contester.id AND
+					    contester.id = ? 
+                                     ORDER BY submits.submitted DESC', array($userid, $contester->id));
     }
     else
     {
-    	$tmp = $DB->get_records_sql('SELECT submits.id as p4, problems.name as p1, languages.name as p2, submits.submitted as p3,
-    		   contester.name as p5
-    	FROM   mdl_contester_problems as problems,
-    		   mdl_contester_submits as submits,
-    		   mdl_contester_languages as languages,
-    		   mdl_contester as contester
-    	WHERE
-    		   submits.student=? AND
-    		   submits.lang=languages.id AND
-    		   submits.problem = problems.dbid AND
-    		   submits.contester = contester.id
-    	ORDER BY submits.submitted DESC', array($userid));    
+        $tmp = $DB->get_records_sql('SELECT submits.id as p4,
+	                                    problems.name as p1,
+					    languages.name as p2,
+					    submits.submitted as p3,
+					    contester.name as p5,
+					    contester.id as contesterID
+                                     FROM   mdl_contester_problems as problems,
+                                            mdl_contester_submits as submits,
+                                            mdl_contester_languages as languages,
+                                            mdl_contester as contester
+                                     WHERE
+                                            submits.student=? AND
+                                            submits.lang=languages.id AND
+                                            submits.problem = problems.dbid AND
+                                            submits.contester = contester.id
+                                     ORDER BY submits.submitted DESC', array($userid));    
     }
     
-	//while ($row = mysql_fetch_array($tmp))
-	foreach($tmp as $row)
-	{
-		$tmpsubmitinfo = contester_get_special_submit_info($row->p4, false, false); //do not return problem name & language info
-		$table->data []= array($row->p1,$row->p2,$row->p3,$tmpsubmitinfo->status,
-			'<a href=show_solution.php?a='.$contester->id.'&sid='.$row->p4.'>'.$tmpsubmitinfo->points.'</a>',
-				       '<a href=view.php?a='.$contester->id.'>'.$row->p5.'</a>');
-	}
+    foreach($tmp as $row)
+    {
+        $tmpsubmitinfo = contester_get_special_submit_info($row->p4, false, false); //do not return problem name & language info
+        $table->data []= array($row->p1,$row->p2,$row->p3,$tmpsubmitinfo->status,
+                               '<a href=show_solution.php?a='.$row->contesterID.'&sid='.$row->p4.'>'.$tmpsubmitinfo->points.'</a>',
+                               '<a href=view.php?a='.$row->contesterID.'>'.$row->p5.'</a>');
+    }
 
-	//print_r($table->data);
-
-	if ($table->data === false)
-	{
-		print_string('nosolutions', contester);
-	} else {
-		echo html_writer::table($table);
-		//echo $OUTPUT->table($newtable);
-		//print_table($table);
-	}
+    if ($table->data === false)
+    {
+        print_string('nosolutions', contester);
+    }
+    else
+    {
+        echo html_writer::table($table);
+    }
 
 /// Finish the page
-	contester_print_end();
+    contester_print_end();
     //print_footer($course);
-	echo $OUTPUT->footer();
+    echo $OUTPUT->footer();
 ?>
