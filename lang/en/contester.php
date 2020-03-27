cf8be1f986b7a86661993b5ebfaa7abf6015baa8
lem <kazachkova.anna@gmail.com>
2020-03-28 01:49:12 +0400

    ;

--- lang/en/contester.php
+++ lang/en/contester.php
@@ -40,9 +40,9 @@ $string['nocontesters'] = 'Нет контестеров';
 $string['modulename_help']        = 'Use the contester module for... | The contester module allows...';
 $string['pluginadministration']   = 'contester administration';
 $string['pluginname']             = 'contester';
-$string['incorrect_contester_id'] = "Неверный id контеста"
-$string['misconfigured_course']   = "Не найден курс, соответствующий контесту"
-$string['incorrect_cm_id']        = "Неверный id модуля"
+$string['incorrect_contester_id'] = "Неверный id контеста";
+$string['misconfigured_course']   = "Не найден курс, соответствующий контесту";
+$string['incorrect_cm_id']        = "Неверный id модуля";
 
 $string['contesterupdate']    = 'Обновить задачи контестера';
 $string['action']             = 'Действие';
