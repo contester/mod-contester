101f9ecd575258b0f92fac113130dcd8a34ca8f4
lem <kazachkova.anna@gmail.com>
2020-03-28 00:22:12 +0400

    checking $id and $a updated

--- view.php
+++ view.php
@@ -25,64 +25,38 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-// Replace contester with the name of your module and remove this line.
+    require('../../config.php');
+    require_once('lib.php');
 
-    require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
-    require_once(dirname(__FILE__).'/lib.php');
-    //require_once(dirname(__FILE__).'/classes/simplehtml_form.php');
-    require_once("$CFG->libdir/formslib.php");
-
-    $id = optional_param('id', 0, PARAM_INT); // Course_module ID, or
-    $a  = optional_param('a', 0, PARAM_INT);  // ... contester instance ID - it should be named as the first character of the module.
+    $id = optional_param('id', 0, PARAM_INT); // Course module ID, or
+    $a  = optional_param('a',  0, PARAM_INT); // ...contester instance ID
 
     if ($id) {
-        if(! $cm = get_coursemodule_from_id('contester', $id))
-    	{
-    		print_error("Course Module ID was incorrect");
-    	}
-        if(! $course = $DB->get_record('course', array('id' => $cm->course)))
-    	{
-    		print_error("Course is misconfigured");
-    	}
-        if(! $contester = $DB->get_record('contester', array('id' => $cm->instance)))
-    	{
-    		print_error("Course module is incorrect");
-    	}
-    } 
-    else 
-    {
-        if(! $contester = $DB->get_record('contester', array('id' => $a)))
-    	{
-    		print_error("Course module is incorrect");
-    	}
-        if(! $course = $DB->get_record('course', array('id' => $contester->course)))
-    	{
-    		print_error("Course is misconfigured");
-    	}
-        if(! $cm = get_coursemodule_from_instance('contester', $contester->id, $course->id))
-    	{
-    		 print_error("Course Module ID was incorrect");
-    	}
+        list ($course, $cm) = get_course_and_cm_from_cmid($id, 'contester');
+        $contester = $DB->get_record('contester', array('id'=> $cm->instance), '*', MUST_EXIST);
+    }
+    else {
+        if(! $contester = $DB->get_record('contester', array('id' => $a))) {
+            print_error(get_string("incorrect_contester_id", "contester"));
+        }
+        if(! $course = $DB->get_record('course', array('id' => $contester->course))) {
+            print_error(get_string("misconfigured_course", "contester"));
+        }
+        if(! $cm = get_coursemodule_from_instance('contester', $contester->id, $course->id)) {
+            print_error(get_string("incorrect_cm_id", "contester"));
+        }
     }
 
     require_login($course->id);
-    //require_login($course, true, $cm);
-
-    /*$event = \mod_contester\event\course_module_viewed::create(array(
-        'objectid' => $PAGE->cm->instance,
-        'context' => $PAGE->context,
-    ));
-    $event->add_record_snapshot('course', $PAGE->course);
-    $event->add_record_snapshot($PAGE->cm->modname, $contester);
-    $event->trigger();*/
 
     // Print the page header.
 
-    $PAGE->set_url('/mod/contester/view.php', array('id' => $cm->id));
+    $PAGE->set_url('/mod/contester/view.php', array('a' => $contester->id));
     $PAGE->set_title(format_string($contester->name));
     $PAGE->set_heading(format_string($course->fullname));
     $PAGE->navbar->add("$contester->name");
-    $PAGE->set_button(update_module_button($cm->id, $course->id, get_string("modulename", "contester")));
+    $PAGE->set_button(update_module_button($cm->id, $course->id,
+                      get_string("modulename", "contester")));
 
     /*
      * Other things you may want to set - remove if not needed.
@@ -99,7 +73,7 @@
     }
 
     contester_print_begin($contester->id, $contester->name);
-    //$sql = "";
+
     $problem_list = $DB->get_recordset_sql("SELECT
 	problemmap.id as id,
 	problems.name as name
@@ -107,52 +81,32 @@
 	{contester_problemmap} problemmap
 	WHERE problemmap.problemid=problems.id and
         problemmap.contesterid=? order by problemmap.id", array($contester->id));
-    if ($problem_list->valid())
-    {
-    	echo "<table width = 90% border=1 bordercolor=black cellpadding=5><tr><td>".get_string('number', 'contester').
-    	"</td><td>".get_string('name','contester')."</td><td>".
-    	get_string('action', 'contester')."</td></tr>";
+    if ($problem_list->valid()) {
+        echo "<table width = 90% border=1 bordercolor=black cellpadding=5><tr><td>".get_string('number', 'contester').
+             "</td><td>".get_string('name','contester')."</td><td>".
+             get_string('action', 'contester')."</td></tr>";
 
-    	$i = 1;
-    	foreach ($problem_list as $problem)
-    	{
-    		echo "<tr valign><td align=center valign=middle>".($i++)."</td><td valign=middle align=left><nobr>".
-	    	$problem->name."</td><td  align=center valign=middle nobr>";
-    		//print_single_button("problem.php", array("pid"=>$problem['id'],"a"=>$contester->id), get_string('definition', 'contester'), 'post');
-    		//contester_print_link_to_problem($contester->id, );
-    		echo "<a href=problem.php?a={$contester->id}&pid={$problem->id}>".
-		   		get_string('problemstatement', 'contester')."</a>";
+        $i = 1;
+        foreach ($problem_list as $problem) {
+            echo "<tr valign><td align=center valign=middle>".($i++)."</td><td valign=middle align=left><nobr>".
+                 $problem->name."</td><td  align=center valign=middle nobr>";
+            echo "<a href=problem.php?a={$contester->id}&pid={$problem->id}>".
+                 get_string('problemstatement', 'contester')."</a>";
+            echo "<form enctype=\"multipart/form-data\" method=\"post\" action=\"submit_form.php?pid={$problem->id}&a={$contester->id}\">";
+            echo '<table cellpadding="5"><tbody>';
+            echo '<tr><td colspan="2" align="center"><input type="submit" value="'.get_string('submit', 'contester').'"></input></td></tr>';
+            echo '</tbody></table></form>';
+            echo "</nobr></td></tr>";
+        }
+        echo "</table>";
+    }
+    else print_string('noproblems', 'contester');
 
-			//print_single_button("", array("pid"=>$problem['id'],"a"=>$contester->id), get_string('submit', 'contester'), 'post');
-			echo "<form enctype=\"multipart/form-data\" method=\"post\" action=\"submit_form.php?pid={$problem->id}&a={$contester->id}\">";
-			echo '<table cellpadding="5"><tbody>';
-		    echo '<tr><td colspan="2" align="center"><input type="submit" value="'.get_string('submit', 'contester').'"></input></td></tr>';
-			echo '</tbody></table></form>';
-
-    		//$form = new moodleform();
-            //$form->url = new moodle_url("submit_form.php", array("pid"=>$problem->id,"a"=>$contester->id)); // Required
-            //$form->button = new html_button();
-            //$form->button->text = get_string('submit', 'contester'); // Required
-            //$form->addElement('button', get_string('submit', 'contester'), get_string('submit', 'contester'));
-            //$form->button->disabled = $disabled;
-            //$form->button->title = $tooltip;
-            //$form->method = 'post';
-            //$form->id = $formid;
-             
-            /*if ($jsconfirmmessage) {
-                $confirmaction = new component_action('click', 'confirm_dialog', array('message' => $jsconfirmmessage));
-                $form->button->add_action($confirmaction);
-            }*/
-             
-            //echo $OUTPUT->button($form);
-	    	echo "</nobr></td></tr>";
-    	}
-    	echo "</table>";
-    } 
-	else print_string('noproblems', 'contester');
     $problem_list->close();
-	contester_print_end();
-//End new code
+
+    contester_print_end();
 
 // Finish the page.
-echo $OUTPUT->footer();
+    echo $OUTPUT->footer();
+
+?>
