ce3e99ebdaf39d25fa0de97303b6a857edf3d937
lem <kazachkova.anna@gmail.com>
2020-03-29 23:13:59 +0400

    in solutions.php

--- my_solutions.php
+++ my_solutions.php
@@ -1,76 +1,43 @@
-<?PHP  // $Id: view.php,v 1.2 2006/04/29 22:19:41 skodak Exp $
+<?PHP
 
-/// This page prints a particular instance of contester
-/// (Replace contester with the name of your module)
+/// This page prints current user's solutions
 
     require_once("../../config.php");
     require_once("lib.php");
 
-    $id = optional_param('id', 0, PARAM_INT); // Course Module ID, or
-    $a  = optional_param('a', 0, PARAM_INT);  // contester ID
+    $a  = required_param('a', PARAM_INT);  // Contester ID
     $thisorall = optional_param('tha', 0 , PARAM_INT); // 0 - this contest, 1 - all submits
-    
-    global $DB;
 
-    if ($id) {
-        if (! $cm = $DB->get_record("course_modules", array("id"=>$id))) {
-            print_error("Course Module ID was incorrect");
-        }
-
-        if (! $course = $DB->get_record("course", array("id"=>$cm->course))) {
-            print_error("Course is misconfigured");
-        }
-
-        if (! $contester = $DB->get_record("contester", array("id"=>$cm->instance))) {
-            print_error("Course module is incorrect");
-        }
-
-    } else {
-        if (! $contester = $DB->get_record("contester", array("id"=>$a))) {
-            print_error("Course module is incorrect");
-        }
-        if (! $course = $DB->get_record("course", array("id"=>$contester->course))) {
-            print_error("Course is misconfigured");
-        }
-        if (! $cm = get_coursemodule_from_instance("contester", $contester->id, $course->id)) {
-            print_error("Course Module ID was incorrect");
-        }
+    if(! $contester = $DB->get_record('contester', array('id' => $a))) {
+        print_error(get_string("incorrect_contester_id", "contester"));
     }
-
-    require_login($course->id);
+    if(! $course = $DB->get_record('course', array('id' => $contester->course))) {
+        print_error(get_string("misconfigured_course", "contester"));
+    }
+    if(! $cm = get_coursemodule_from_instance('contester', $contester->id, $course->id)) {
+        print_error(get_string("incorrect_cm_id", "contester"));
+    }
 
     //add_to_log($course->id, "contester", get_string('mysolutions', 'contester'), "my_solutions.php?a=$contester->id", "$contester->id");
 
 /// Print the page header
 
-	/*
-    if ($course->category) {
-        $navigation = "<a href=\"../../course/view.php?id=$course->id\">$course->shortname</a> ->";
-    }
+    $PAGE->set_url('/mod/contester/my_solutions.php', array('a' => $contester->id));
+    $PAGE->set_title("$course->shortname: $contester->name");
+    $PAGE->set_heading("$course->fullname");
 
-    $strcontesters = get_string("modulenameplural", "contester");
-    $strcontester  = get_string("modulename", "contester");
-
-    print_header("$course->shortname: $contester->name", "$course->fullname",
-                 "$navigation <a href=index.php?id=$course->id>$strcontesters</a> -> $contester->name",
-                  "", "<link rel=\"stylesheet\" type=\"text/css\" href=\"css/styles.css\" />", true, update_module_button($cm->id, $course->id, $strcontester),
-                  navmenu($course, $cm));
-	*/
-	
-    $PAGE->set_url('/mod/contester/my_solutions.php', array('id' => $cm->id));
-    $PAGE->set_title(format_string($contester->name));
-    $PAGE->set_heading(format_string($course->fullname));
-
-    $contester_url = new moodle_url('/mod/contester/view.php', array('a' => $a));
+    $contester_url = new moodle_url('/mod/contester/view.php', array('a' => $contester->id));
     $PAGE->navbar->add("$contester->name", $contester_url);
-    $PAGE->set_button(update_module_button($cm->id, $course->id, get_string("modulename", "contester")));	
+    $PAGE->set_button(update_module_button($cm->id, $course->id,
+                      get_string("modulename", "contester")));
+
+    echo $OUTPUT->header();
+
 
 /// Print the main part of the page
 
-    echo $OUTPUT->header();
+
     contester_print_begin($contester->id);
-    // header
-    echo "<br />";
 
     // print the number of solutions in queue
     $qnum = $DB->get_record_sql("SELECT  COUNT(1) as cnt
@@ -81,24 +48,19 @@
                                  WHERE   (processed = 255)");
     echo "<p>".get_string("numinqueue", "contester").": ".$qnum->cnt.
           " (".get_string("numchecked", "contester")." ". $cnum->cnt.")";
-    if ($is_admin)
-        echo "<a href=status.php?a=$instance>".get_string('status', 'contester')."</a>";
-    echo "</p>";
 
     $thisc = get_string('thiscontester', 'contester');
     $allc = get_string('all', 'contester');
 
-    if ($thisorall == 1)
-    {
+    if ($thisorall == 1) {
         $thisc = "<a href=my_solutions.php?a=".$contester->id."&tha=0>".$thisc."</a>";
     }
-    else
-    {
+    else {
         $allc = "<a href=my_solutions.php?a=".$contester->id."&tha=1>".$allc."</a>";
     }
     echo "<p><strong>".get_string('solutionlist', 'contester')." (".$thisc." \ ".$allc.")</strong></p>";
 
-    if (!$userid) 
+    if (!$userid)
         $userid = $USER->id;
 
     $table = new html_table();
@@ -106,26 +68,7 @@
                          get_string('date'), get_string('status', 'contester'), get_string('points', 'contester'),
                          get_string('modulename', 'contester'));
 
-	/*$sql = "
-	SELECT problems.name as p1, languages.name as p2, submits.submitted as p3,
-		   submits.id as p4, contester.name as p5
-	FROM   mdl_contester_problems as problems,
-		   mdl_contester_submits as submits,
-		   mdl_contester_languages as languages,
-		   mdl_contester as contester
-	WHERE
-		   submits.student=$userid AND
-		   submits.lang=languages.id AND
-		   submits.problem = problems.dbid AND
-		   submits.contester = contester.id".$thiscontester."
-	ORDER BY submits.submitted DESC
-	";
-	//echo "<textarea>".$sql."</textarea>";
-
-	$tmp = mysql_query($sql);*/
-	
-    if ($thisorall != 1)	
-    {
+    if ($thisorall != 1) {
         $tmp = $DB->get_records_sql('SELECT submits.id as p4,
 	                                    submits.contester as p6,
 	                                    problems.name as p1,
@@ -141,11 +84,10 @@
                                             submits.lang=languages.id AND
                                             submits.problem = problems.dbid AND
                                             submits.contester = contester.id AND
-					    contester.id = ? 
+					    contester.id = ?
                                      ORDER BY submits.submitted DESC', array($userid, $contester->id));
     }
-    else
-    {
+    else {
         $tmp = $DB->get_records_sql('SELECT submits.id as p4,
 	                                    submits.contester as p6,
 	                                    problems.name as p1,
@@ -163,7 +105,7 @@
                                             submits.contester = contester.id
                                      ORDER BY submits.submitted DESC', array($userid));    
     }
-    
+
     foreach($tmp as $row)
     {
         $tmpsubmitinfo = contester_get_special_submit_info($row->p4, false, false); //do not return problem name & language info
@@ -172,17 +114,19 @@
                                '<a href=view.php?a='.$row->p6.'>'.$row->p5.'</a>');
     }
 
-    if ($table->data === false)
-    {
+    if ($table->data === false) {
         print_string('nosolutions', contester);
     }
-    else
-    {
+    else {
         echo html_writer::table($table);
     }
 
-/// Finish the page
+
     contester_print_end();
-    //print_footer($course);
+
+
+/// Finish the page
+
     echo $OUTPUT->footer();
+
 ?>
