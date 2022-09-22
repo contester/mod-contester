6dcfc455b29f7760158edcfcf65eda96bb6977fc
Lem-a-k <kazachkova.anna@gmail.com>
2022-09-23 02:41:52 +0400

    show "unconnected" submits
    deleted contests and deleted from contester problemmap problems

--- my_solutions.php
+++ my_solutions.php
@@ -86,41 +86,88 @@
                                             problemmap.contesterid = contester.id AND
                                             problemmap.problemid = problems.id
                                      ORDER BY submits.submitted_uts DESC', [$userid, $contester->id]);
+        foreach($tmp as $row) {
+            $tmpsubmitinfo = contester_get_special_submit_info($row->p4, false, false); //do not return problem name & language info
+            $url_problem = new moodle_url('problem.php', ['a' => $row->a, 'pid' => $row->pid]);
+            $table->data []= array('<a href='.$url_problem.'>'.$row->p1.'</a>',
+                                   $row->p2,
+                                   userdate($row->submitted_uts, get_string('strftimedatetime')),
+                                   $tmpsubmitinfo->status,
+                                   '<a href=show_solution.php?a='.$row->a.'&sid='.$row->p4.'>'.$tmpsubmitinfo->points.'</a>',
+                                   '<a href=view.php?a='.$row->a.'>'.$row->p5.'</a>');
+        }
     }
     else {
-        $tmp = $DB->get_records_sql('SELECT submits.id as p4,
-	                                    submits.contester as a,
-	                                    problems.name as p1,
-					    languages.name as p2,
-					    submits.submitted_uts as submitted_uts,
-					    contester.name as p5,
-                                            problemmap.id as pid
+        $tmp = $DB->get_records_sql('SELECT submits.id as submit_id,
+	                                    problems.name as problem_name,
+					    languages.name as language_name,
+					    submits.submitted_uts as submitted_uts
                                      FROM   {contester_problems} as problems,
 					    {contester_submits} as submits,
-					    {contester_languages} as languages,
-					    {contester} as contester,
-					    {contester_problemmap} as problemmap
+					    {contester_languages} as languages
                                      WHERE
                                             submits.student=? AND
                                             submits.lang=languages.id AND
-                                            submits.problem = problems.dbid AND
-                                            submits.contester = contester.id AND
-                                            problemmap.contesterid = contester.id AND
-                                            problemmap.problemid = problems.id
+                                            submits.problem = problems.dbid
                                      ORDER BY submits.submitted_uts DESC', [$userid]);
+        foreach($tmp as $row) {
+            $contester = $DB->get_records_sql('SELECT submits.contester as a,	                                    
+    		                                      contester.name as contester_name
+                                               FROM   {contester_submits} as submits,
+                                                      {contester} as contester
+                                               WHERE
+                                                      submits.contester = contester.id AND
+					              submits.id = ?', [$row->submit_id]);
+            if ($contester) {
+                $row->a = $contester[0]->a;
+                $row->contester_name = $contester[0]->contester_name;
+
+                $problemmap = $DB->get_records_sql('SELECT problemmap.id as pid
+                                                    FROM   {contester_problems} as problems,
+                                                           {contester_submits} as submits,
+                                                           {contester_problemmap} as problemmap
+                                                    WHERE
+                                                           submits.problem = problems.dbid AND
+                                                           problemmap.contesterid = submits.contester AND
+                                                           problemmap.problemid = problems.id AND
+                                                           submits.id = ?', [$row->submit_id]);
+                if ($problemmap) {
+                    $row->pid = $problemmap[0]->pid;
+                }
+	    }
+
+            if (!isset ($row->a)) {
+                $problem = $row->problem_name;
+                $status = "[deleted contest]";
+                $testing = '<a href=show_solution.php?a='.$a.'&sid='.$row->submit_id.'>'."[deleted contest]".'</a>';
+                $solution = '<a href=view.php?a='.$a.'>'.$row->contester_name.'</a>';
+	    }
+            else if (!isset ($row->pid)) {
+                $problem = $row->problem_name;
+                $status = "[deleted problem]";
+                $testing = '<a href=show_solution.php?a='.$row->a.'&sid='.$row->submit_id.'>'."[deleted problem]".'</a>';
+                $solution = '<a href=view.php?a='.$row->a.'>'.$row->contester_name.'</a>';
+	    }
+            else {
+                $url_problem = new moodle_url('problem.php', ['a' => $row->a, 'pid' => $row->pid]);
+                $problem = '<a href='.$url_problem.'>'.$row->problem_name.'</a>';
+    
+                $tmpsubmitinfo = contester_get_special_submit_info($row->submit_id, false, false); //do not return problem name & language info
+                $status = $tmpsubmitinfo->status;
+                $testing = '<a href=show_solution.php?a='.$row->a.'&sid='.$row->submit_id.'>'.$tmpsubmitinfo->points.'</a>';
+                $solution = '<a href=view.php?a='.$row->a.'>'.$row->contester_name.'</a>';
+	    }
+
+            $table->data []= array($problem,
+                                   $row->language_name,
+                                   userdate($row->submitted_uts, get_string('strftimedatetime')),
+                                   $status,
+                                   $testing,
+                                   $solution);
+
+	}
     }
 
-    foreach($tmp as $row)
-    {
-        $tmpsubmitinfo = contester_get_special_submit_info($row->p4, false, false); //do not return problem name & language info
-        $url_problem = new moodle_url('problem.php', ['a' => $row->a, 'pid' => $row->pid]);
-        $table->data []= array('<a href='.$url_problem.'>'.$row->p1.'</a>',
-                               $row->p2,
-                               userdate($row->submitted_uts, get_string('strftimedatetime')),
-                               $tmpsubmitinfo->status,
-                               '<a href=show_solution.php?a='.$row->a.'&sid='.$row->p4.'>'.$tmpsubmitinfo->points.'</a>',
-                               '<a href=view.php?a='.$row->a.'>'.$row->p5.'</a>');
-    }
 
     if ($table->data === false) {
         print_string('nosolutions', contester);
